88539dd0d61ab7bfc23c20bab33dc020eaa881b8
selvakumaran2k <68019336+selvakumaran2k@users.noreply.github.com>
2021-03-22 10:43:57 +0530

    working_with_output

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,9 @@
 #define trigPin 3 //attach pin D3 Arduino to pin Trig of HC-SR04
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
-// defines variables
-long duration; // variable for the duration of sound wave travel
-int distance;
 #include<Arduino.h>
 #include <Servo.h>
+//functions
 void scan();
 int angleToPulse(int ang);
 void p(int rawoutput[16]);
@@ -23,19 +21,19 @@ void project(int rawoutput[16]);
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 #define SERVOMIN  155 // this is the 'minimum' pulse length count (out of 4096)
 #define SERVOMAX  555 // this is the 'maximum' pulse length count (out of 4096)
-
+long duration; // variable for the duration of sound wave travel
+int distance;
 // our servo # counter
 uint8_t servonum = 0;
-
 Servo myservox;
 Servo myservoy; // create servo object to control a servo
 // twelve servo objects can be created on most boards
-
+//variables
 int posx = 40;    // variable to store the servo position
 int posy = 40;
 int stepx = 5;
 int stepy = 5;
-int t =30;
+int t =10;
 int row = 0;
 int rawoutput[16]={-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
 int computed_matrix[16]={-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
@@ -79,7 +77,8 @@ void loop() {
     }
   }
   p(rawoutput);
-  project(rawoutput);
+  compute(rawoutput);
+  project(computed_matrix);
   row = 0;
 }
 void p(int rawoutput[16])
@@ -106,41 +105,32 @@ void p(int rawoutput[16])
    duration = pulseIn(echoPin, HIGH);
    // Calculating the distance
    distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
+   if(distance>800)
+   {
+     distance = 800;
+   }
    rawoutput[row++] = distance;
  }
 
 int angleToPulse(int ang){
    int pulse = map(ang,0, 180, SERVOMIN,SERVOMAX);// map angle of 0 to 180 to Servo min and Servo max 
-   Serial.print("Angle: ");Serial.print(ang);
-   Serial.print(" pulse: ");Serial.println(pulse);
+  //  Serial.print("Angle: ");Serial.print(ang);
+  //  Serial.print(" pulse: ");Serial.println(pulse);
    return pulse;
 }
 void project(int computed_matrix[16]){
   for (int servo_and_mat = 0; servo_and_mat < 16;servo_and_mat++)
   {
-    delay(500);
       pwm.setPWM(servo_and_mat, 0, angleToPulse(computed_matrix[servo_and_mat]));
+      delay(50);//servo mechanical delay
   }
 }
 void compute(int rawoutput[16])
 {
-  //calculating max and min
-  int max=0;
-  int min=99999;
-  for (int ite = 0; ite < 16;ite++)
-  {
-    if(max<rawoutput[ite])
-    {
-      max = rawoutput[ite];
-    }
-    if(min>rawoutput[ite])
-    {
-      min = rawoutput[ite];
-    }
-  }
+  //raw mapping
   for (int i = 0; i < 16;i++)
   {
-    int var = map(rawoutput[i],0,500, min, max);
+    int var = map(rawoutput[i],0,800, 0, 180);
     computed_matrix[i] = var;
   }
   Serial.println("Computed Matrix");
